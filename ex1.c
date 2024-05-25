a62e136ddba01d7c9235d343df8ada213989d80e
laith <laithmimi03@gmail.com>
2024-05-25 02:18:43 -0500

    first commit  it has an error, not completed..

---
+++ ex1.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_CMD_LEN 1024
+#define MAX_ARGS 4
+
+
+void displayPrompt(int numOfCmd,int ActiveAliases,int ScriptLines){
+    printf("#cmd:%d|#alias:%d|#script lines:%d>",numOfCmd,ActiveAliases,ScriptLines);
+}
+
+int main() {
+    char *cmd = (char *) malloc(MAX_CMD_LEN * sizeof(char));
+    char *argv[MAX_ARGS + 1]; //+1 for the NULL terminator
+
+    const char *delim = " \t\n";
+
+    int numOfCmd = 0;
+
+    int ActiveAliases = 0;
+    int ScriptLines = 0;
+    pid_t PID;
+        if(cmd==NULL){
+            perror("Error: Memory Allocation failed");
+            return 1;
+        }
+    while (1) {
+        displayPrompt(numOfCmd, ActiveAliases, ScriptLines);
+
+                                             /*READ COMMANDS*/
+        if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL) {
+            perror("Error reading input");
+            continue;
+        }
+        // Remove trailing newline character if any
+        if (strlen(cmd) > 0 && cmd[strlen(cmd) - 1] == '\n') {
+            cmd[strlen(cmd) - 1] = '\0';
+        }
+
+        if (strcmp(cmd, "exit") == 0) {
+            //free(cmd);
+            break;
+        }
+                                            /*PARSE COMMANDS*/
+        char *token = strtok(cmd, delim);
+        int arg_count = 0;
+        //loop that reads input and then parses it into tokens,stores them in an cmd_array up to a maximum count
+        while (token != NULL) {
+                if (arg_count <= MAX_ARGS) {
+                    argv[arg_count] = token;
+                    arg_count++;
+                }else{
+                    fprintf(stderr, "Illegal Command: Too Many Arguments\n");
+                    arg_count = 0; // Reset argument count to avoid partially filled argv
+                    break;
+                }
+                token = strtok(NULL, delim);
+            }
+            // Null-terminate the argument list
+            argv[arg_count]=NULL;
+            if(arg_count==0) continue; //continue if no cmd found
+
+            if (arg_count > MAX_ARGS) {
+                perror("Illegal Command: Too Many Arguments\n");
+                continue;
+            }
+            if (strlen(cmd) > MAX_CMD_LEN) {
+                perror("Illegal Command: Too Many Characters");
+                continue;
+            }
+
+        PID = fork();
+        if (PID == -1) {
+            perror("Error: fork failed");
+            free(cmd);
+            exit(EXIT_FAILURE);;
+        }
+        else if (PID == 0) { //child process
+                                            /*EXECUTE COMMANDS*/
+            if (execvp(argv[0], argv) == -1) {
+                fprintf(stderr, "\nError executing command %s\n", &cmd[0]);
+                continue;
+            }
+            if (strcmp(cmd, "exit") != 0) {
+                perror("Error: Command not found");
+                continue;
+            }
+
+        }
+        else { //parent process
+
+            int status;
+            if (waitpid(PID, &status, 0) < 0) {
+                perror("Error: waitpid");
+                free(cmd);
+                continue;
+            }
+
+            //check return status
+            if (WEXITSTATUS(status) != 0)
+                return 1;
+            numOfCmd++;
+        }
+
+    }
+    free(cmd);
+    return 0;
+}
