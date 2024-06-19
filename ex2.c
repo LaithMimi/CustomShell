53949849b8cf3119e35fb1037a7251ad43061156
LaithMimi <laithmimi03@gmail.com>
2024-06-19 18:27:44 -0500

    Operators with & updated

--- ex2.c
+++ ex2.c
@@ -12,6 +12,11 @@
 static int numOfCmd = 0, quotesNum = 0,ErrorFlag =0,
         background=0, jobsCounter = 0;
 
+typedef struct {
+    char cmd[MaxCmdLen];
+} Job;
+
+Job jobList[10];
 
 void displayPrompt();
 void handleCmd(char *cmd, char ch);
@@ -19,8 +24,8 @@ int pairsOfQuotes(char *token, char ch);
 void cmdExecution(char **argv);
 void processOperators(char *cmd);
 void handle_background(char *cmd);
-void sigchld_handler(int sig);
-
+//void sigchld_handler(int sig);
+void displayJobs();
 
 int main() {
     char *cmd = (char*)malloc(MaxCmdLen * sizeof(char));
@@ -30,16 +35,7 @@ int main() {
         perror("Error: Memory Allocation failed");
         return 1;
     }
-    // Set up signal handler for SIGCHLD
-    struct sigaction sa;
-    sa.sa_handler = &sigchld_handler;
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        free(cmd);
-        exit(EXIT_FAILURE);
-    }
+
     while (1) {
         displayPrompt();
 
@@ -53,7 +49,10 @@ int main() {
             printf("The number of quotes is: %d\n", quotesNum);
             break;
         }
-
+        if (strcmp(cmd, "jobs\n") == 0) {
+            displayJobs();
+            continue;
+        }
         handleCmd(cmd, ch);
     }
 
@@ -61,12 +60,18 @@ int main() {
     return 0;
 }
 
+void displayJobs() {
+    for (int i = 0; i < jobsCounter; i++) {
+        printf("[%d]   Running               %s\n", i+1,  jobList[i].cmd);
+    }
+}
+
 void displayPrompt() {
     printf("#cmd:%d|#alias:%d|#script lines:%d>", numOfCmd, 0, 0);
 }
 
 void handleCmd(char *cmd, char ch) {
-    // Remove trailing newline character
+    //remove trailing newline character
     if (strlen(cmd) > 0 && cmd[strlen(cmd) - 1] == '\n') {
         cmd[strlen(cmd) - 1] = '\0';
     }
@@ -84,6 +89,7 @@ void handleCmd(char *cmd, char ch) {
         handle_background(cmd);
         return;
     }
+
     char *delim = " =";
     char *token = strtok(cmd, delim);
     char **argv = (char **)malloc((MaxArg + 1) * sizeof(char *));
@@ -94,6 +100,7 @@ void handleCmd(char *cmd, char ch) {
     }
 
     int argCount = 0;
+
     while (token != NULL) {
         if (argCount <= MaxArg) {
             argv[argCount] = token;
@@ -116,7 +123,6 @@ void handleCmd(char *cmd, char ch) {
         free(argv);
         return;
     }
-
     cmdExecution(argv);
     free(argv);
 }
@@ -137,55 +143,52 @@ void processOperators(char *cmd) {
     char *commands[MaxArg + 1];
     int cmdCount = 0;
 
-    // Handle '&&' operators first
-    token = strtok(cmd, "&&");
+    // First, tokenize by '||'
+    token = strtok(cmd, "||");
     while (token != NULL) {
         commands[cmdCount++] = token;
-        token = strtok(NULL, "&&");
+        token = strtok(NULL, "||");
         if (cmdCount > MaxArg) {
-            perror("ERR: Too many commands");
+            perror("ERR");
             exit(EXIT_FAILURE);
         }
     }
 
     for (int i = 0; i < cmdCount; i++) {
-        char *subToken;
-        char *subCommands[MaxArg + 1];
-        int subCmdCount = 0;
+        if (!strstr(commands[i], "&&")) { // Command without '&&'
+            handleCmd(commands[i], '"');
+//
+        } else { // Command with '&&'
+            char *tmpToken;
+            char *tmpCommands[MaxArg + 1];
+            int tmpCmdCount = 0;
 
-        // Check if the current command contains '||'
-        if (strstr(commands[i], "||")) {
-            subToken = strtok(commands[i], "||");
-            while (subToken != NULL) {
-                subCommands[subCmdCount++] = subToken;
-                subToken = strtok(NULL, "||");
-                if (subCmdCount > MaxArg) {
-                    perror("ERR: Too many sub-commands");
-                    exit(EXIT_FAILURE);
+            tmpToken = strtok(commands[i], "&&");
+            while (tmpToken != NULL) {
+                tmpCommands[tmpCmdCount++] = tmpToken;
+                tmpToken = strtok(NULL, "&&");
+            }
+
+            for (int j = 0; j < tmpCmdCount; j++) {
+                handleCmd(tmpCommands[j], '"');
+                if (ErrorFlag) {
+                    break; // Stop processing further commands in this group
                 }
             }
 
-            for (int j = 0; j < subCmdCount; j++) {
-                handleCmd(subCommands[j], '"');
+            if (!ErrorFlag) { // If all commands in this group succeeded
+                break; // Stop processing further '||' groups
             }
         }
-
-        else {
-            handleCmd(commands[i], '"');
-            if (ErrorFlag) {  // Stop if a command failed in '&&'
-                return;
-            }
-        }
+        ErrorFlag = 0; // Reset the error flag for the next group of commands
     }
-
-    ErrorFlag = 0; // Reset the error flag after processing
 }
+
 
 void cmdExecution(char **argv) {
     pid_t PID = fork();
     if (PID == -1) {
         perror("ERR");
-        ErrorFlag = 1;
         exit(EXIT_FAILURE);
     }
     else if (PID == 0) { // Child process
@@ -199,7 +202,7 @@ void cmdExecution(char **argv) {
             int status;
             wait(&status);
             if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-                (numOfCmd)++; // Increment only if the command executed successfully
+                (numOfCmd)++; //increment only if the command executed successfully
                 ErrorFlag = 0;
             } else {
                 ErrorFlag = 1;
@@ -212,22 +215,24 @@ void cmdExecution(char **argv) {
     }
 }
 
-void sigchld_handler(int sig) {
-    pid_t pid;
-    int status;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        printf("Child process %d terminated\n", pid);
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-            // Increment the number of commands executed successfully
-            numOfCmd++;
-        } else {
-            // Set the error flag if the child process terminated with an error
-            ErrorFlag = 1;
-        }
-    }
-}
+//void sigchld_handler(int sig) {
+//    pid_t pid;
+//    int status;
+//    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+//        usleep(100000);
+//        //printf("Child process %d terminated\n", pid);
+//        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+//            // Increment the number of commands executed successfully
+//            numOfCmd++;
+//        } else {
+//            // Set the error flag if the child process terminated with an error
+//            ErrorFlag = 1;
+//        }
+//    }
+//}
 
 void handle_background(char *cmd) {
+    strcpy(jobList[jobsCounter].cmd,cmd);
     // Remove the '&' character from the command
     cmd[strlen(cmd) - 1] = '\0';
 
