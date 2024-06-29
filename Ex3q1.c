32502f0ffaca0a902e6aeeab1f30607dbe51c59b
LaithMimi <119529431+LaithMimi@users.noreply.github.com>
2024-06-29 12:23:30 +0300

    Update Ex3q1.c

--- Ex3q1.c
+++ Ex3q1.c
@@ -3,19 +3,30 @@
 #include <string.h>
 #include <complex.h>
 #include "math.h"
+
 #define MAX_SIZE 128
 
-int matrixType(complex double **matrix,const int *rows, const int *cols);
+int matrixType(complex double **matrix, const int *rows, const int *cols);
+
 complex double **inputMatrix(char input[MAX_SIZE], int *rows, int *cols);
+
 complex double **createMatrix(int rows, int cols);
 void freeMatrix(complex double **matrix, int rows);
-complex double **ADDMatrices(complex double** firMatrix,complex double** secMatrix, int rows, int cols);
+
+complex double **ADDMatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols);
+
 complex double **SUBMatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols);
+
 complex double **MULMatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols);
+
 complex double **TRANSPOSEMatrices(complex double **firMatrix, int rows, int cols);
+
 complex double **logANDmatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols);
+
 complex double **logORmatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols);
+
 complex double **logNOTmatrices(complex double **matrix, int rows, int cols);
+
 void printMatrix(complex double **matrix, int rows, int cols);
 
 
@@ -23,67 +34,91 @@ int main() {
     double complex **firMatrix, **secMatrix, **resMatrix;
     char op[10], input[MAX_SIZE];
     int rows1, cols1, rows2, cols2;
-    while (1) {
-        printf("Enter the first matrix in format (rows,columns:val1,val2,...,valN): ");
-        scanf(" %[^\n]", input);
-        firMatrix = inputMatrix(input, &rows1, &cols1);
 
-        printf("Enter the second matrix in format (rows,columns:val1,val2,...,valN): ");
-        scanf(" %[^\n]", input);
-        secMatrix = inputMatrix(input, &rows2, &cols2);
+    //while (1) {
+    //printf("Enter the first matrix in format (rows,columns:val1,val2,...,valN): ");
+    scanf(" %[^\n]", input);
+    firMatrix = inputMatrix(input, &rows1, &cols1);
 
-        printf("Enter the operation (ADD,SUB,MUL,AND,OR,NOT,TRANSPOSE): ");
-        scanf("%s", op);
+    //printf("Enter the second matrix in format (rows,columns:val1,val2,...,valN): ");
+    scanf(" %[^\n]", input);
+    secMatrix = inputMatrix(input, &rows2, &cols2);
 
+    //printf("Enter the operation (ADD,SUB,MUL,AND,OR,NOT,TRANSPOSE): ");
+    scanf("%s", op);
 
-        // Perform the operation
-        if (strcmp(op, "ADD") == 0) {
-            resMatrix = ADDMatrices(firMatrix, secMatrix, rows1, cols1);
-        } else if (strcmp(op, "SUB") == 0) {
-            resMatrix = SUBMatrices(firMatrix, secMatrix, rows1, cols1);
-        } else if (strcmp(op, "MUL") == 0) {
-            resMatrix = MULMatrices(firMatrix, secMatrix, rows1, cols1);
-        } else if (strcmp(op, "TRANSPOSE") == 0) {
-            resMatrix = TRANSPOSEMatrices(firMatrix, rows1, cols1);
-        } else if (strcmp(op, "AND") == 0) {
-            resMatrix = logANDmatrices(firMatrix, secMatrix, rows1, cols1);
-        } else if (strcmp(op, "OR") == 0) {
-            resMatrix = logORmatrices(firMatrix, secMatrix, rows1, cols1);
-        } else if (strcmp(op, "NOT") == 0) {
-            resMatrix = logNOTmatrices(firMatrix, rows1, cols1);
-        } else {
-            printf("NOT VALID OP\n");
-        }
-
-        printf("Output: ");
-        printMatrix(resMatrix, rows1, cols1);
-
-        // Free memory
-        freeMatrix(firMatrix, rows1);
-        freeMatrix(secMatrix, rows2);
-        if (strcmp(op, "TRANSPOSE") == 0) {
-            freeMatrix(resMatrix, cols1);
-        } else {
-            freeMatrix(resMatrix, rows1);
-        }
-        if(strcmp(op,"Exit")==0)break;
+/*        if (strcmp(op, "Exit") == 0) {
+//            // Free memory
+//            freeMatrix(firMatrix, rows1);
+//            freeMatrix(secMatrix, rows2);
+//            if (strcmp(op, "TRANSPOSE") == 0) {
+//                freeMatrix(resMatrix, cols1);
+//            } else {
+//                freeMatrix(resMatrix, rows1);
+//            }
+//            break;
+        }*/
+    // Perform the operation
+    if (strcmp(op, "ADD") == 0) {
+        resMatrix = ADDMatrices(firMatrix, secMatrix, rows1, cols1);
+    } else if (strcmp(op, "SUB") == 0) {
+        resMatrix = SUBMatrices(firMatrix, secMatrix, rows1, cols1);
+    } else if (strcmp(op, "MUL") == 0) {
+        resMatrix = MULMatrices(firMatrix, secMatrix, rows1, cols1);
+    } else if (strcmp(op, "TRANSPOSE") == 0) {
+        resMatrix = TRANSPOSEMatrices(firMatrix, rows1, cols1);
+    } else if (strcmp(op, "AND") == 0) {
+        resMatrix = logANDmatrices(firMatrix, secMatrix, rows1, cols1);
+    } else if (strcmp(op, "OR") == 0) {
+        resMatrix = logORmatrices(firMatrix, secMatrix, rows1, cols1);
+    } else if (strcmp(op, "NOT") == 0) {
+        resMatrix = logNOTmatrices(firMatrix, rows1, cols1);
+    } else {
+        printf("NOT VALID OP\n");
     }
+
+    //printf("Output: ");
+    printMatrix(resMatrix, rows1, cols1);
+
+    freeMatrix(firMatrix, rows1);
+    freeMatrix(secMatrix, rows2);
+    if (strcmp(op, "TRANSPOSE") == 0) {
+        freeMatrix(resMatrix, cols1);
+    } else {
+        freeMatrix(resMatrix, rows1);
+    }
+
+    // }
 
     return 0;
 }
-int matrixType(complex double **matrix,const int *rows, const int *cols){
+
+int matrixType(complex double **matrix, const int *rows, const int *cols) {
+    int hasComplex = 0;
+    int hasDouble = 0;
+
     for (int i = 0; i < *rows; i++) {
         for (int j = 0; j < *cols; j++) {
             double real = creal(matrix[i][j]);
             double imag = cimag(matrix[i][j]);
 
-            if ((floor(real) != real) || imag != 0.0) {
-                return 0; // Not an integer matrix
+            if (imag != 0.0) {
+                hasComplex = 1;
+            } else if (floor(real) != real) {
+                hasDouble = 1;
             }
         }
     }
-    return 1; // Integer matrix
+
+    if (hasComplex) {
+        return 1;
+    } else if (hasDouble) {
+        return 3;
+    } else {//integer numbers
+        return 0;
+    }
 }
+
 complex double **logNOTmatrices(complex double **matrix, int rows, int cols) {
     complex double **result = createMatrix(rows, cols);
     for (int i = 0; i < rows; i++) {
@@ -93,6 +128,7 @@ complex double **logNOTmatrices(complex double **matrix, int rows, int cols) {
     }
     return result;
 }
+
 
 complex double **logORmatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols) {
     complex double **result = createMatrix(rows, cols);
@@ -105,7 +141,7 @@ complex double **logORmatrices(complex double **firMatrix, complex double **secM
 }
 
 complex double **logANDmatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols) {
-    complex double ** result = createMatrix(rows, cols);
+    complex double **result = createMatrix(rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             result[i][j] = (cabs(firMatrix[i][j]) != 0 && cabs(secMatrix[i][j]) != 0) ? 1.0 : 0.0;
@@ -115,7 +151,7 @@ complex double **logANDmatrices(complex double **firMatrix, complex double **sec
 }
 
 complex double **TRANSPOSEMatrices(complex double **firMatrix, int rows, int cols) {
-    complex double** result = createMatrix(cols, rows);
+    complex double **result = createMatrix(cols, rows);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             result[j][i] = firMatrix[i][j];
@@ -125,10 +161,10 @@ complex double **TRANSPOSEMatrices(complex double **firMatrix, int rows, int col
 }
 
 complex double **MULMatrices(complex double **firMatrix, complex double **secMatrix, int rows, int cols) {
-    complex double** result = createMatrix(rows, cols);
+    complex double **result = createMatrix(rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            result[i][j] = 0+0*I;
+            result[i][j] = 0 + 0 * I;
             for (int k = 0; k < cols; k++) {
                 result[i][j] += firMatrix[i][k] * secMatrix[k][j];
             }
@@ -137,16 +173,17 @@ complex double **MULMatrices(complex double **firMatrix, complex double **secMat
     return result;
 }
 
-complex double **SUBMatrices(double complex** firMatrix, double complex** secMatrix, int rows, int cols) {
-    complex double** result = createMatrix(rows, cols);
+complex double **SUBMatrices(double complex **firMatrix, double complex **secMatrix, int rows, int cols) {
+    complex double **result = createMatrix(rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             result[i][j] = firMatrix[i][j] - secMatrix[i][j];
         }
     }
-    return result;}
+    return result;
+}
 
-complex double **ADDMatrices(double complex** firMatrix, double complex** secMatrix, int rows, int cols) {
+complex double **ADDMatrices(double complex **firMatrix, double complex **secMatrix, int rows, int cols) {
     complex double **result = createMatrix(rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -156,37 +193,46 @@ complex double **ADDMatrices(double complex** firMatrix, double complex** secMat
     return result;
 }
 
-complex double ** createMatrix(int rows, int cols) {
-    complex double **matrix = (double complex**)malloc(rows * sizeof(double complex*));
+complex double **createMatrix(int rows, int cols) {
+    complex double **matrix = (complex double **) malloc(rows * sizeof(complex double *));
     for (int i = 0; i < rows; i++) {
-        matrix[i] = (double complex*)malloc(cols * sizeof(double complex));
+        matrix[i] = (complex double *) malloc(cols * sizeof(complex double));
     }
     return matrix;
 }
-void freeMatrix(complex double **matrix, int rows){
+
+void freeMatrix(complex double **matrix, int rows) {
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
 }
 
-complex double **inputMatrix(char input[MAX_SIZE], int *rows, int *cols){
+complex double **inputMatrix(char input[MAX_SIZE], int *rows, int *cols) {
     sscanf(input, "(%d,%d:", rows, cols);
-    complex double** matrix = createMatrix(*rows, *cols);
-    char* token = strtok(input, ":");
+    complex double **matrix = createMatrix(*rows, *cols);
+    char *token = strtok(input, ":");
     token = strtok(NULL, ",");
     for (int i = 0; i < *rows; i++) {
         for (int j = 0; j < *cols; j++) {
             double real = 0, imag = 0;
-            char* plus = strchr(token, '+');
-            char* i_char = strchr(token, 'i');
-            if (plus && i_char) {
+            char *plus = strchr(token, '+');
+            char *minus = strchr(token, '-');
+            char *i_char = strchr(token, 'i');
+
+            if ((plus || minus) && i_char) {
                 // Complex number
-                real = strtod(token, &token);
-                imag = strtod(token + 1, &token); // Skip '+'
+                sscanf(token, "%lf%lf", &real, &imag);
             } else if (i_char) {
                 //imaginary number
-                imag = strtod(token, &token); // Skip 'i'
+                if (token == i_char)
+                    // Input is only 'i', should be interpreted as '1i'
+                    imag = 1.0;
+                else if (token + 1 == i_char) {
+                    imag = -1.0;
+                } else
+                    sscanf(token, "%lf", imag);
+
             } else {
                 // Real number (integer or double)
                 real = strtod(token, &token);
@@ -200,22 +246,26 @@ complex double **inputMatrix(char input[MAX_SIZE], int *rows, int *cols){
 
 void printMatrix(complex double **matrix, int rows, int cols) {
     printf("(%d,%d:", rows, cols);
+    int type = matrixType(matrix, &rows, &cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             double real = creal(matrix[i][j]);
             double imag = cimag(matrix[i][j]);
 
-            if (matrixType(matrix, &rows,&cols) && imag == 0.0) {
-                // Print as integer
+            if (type == 0) {
+                // Integer
                 printf("%d", (int) real);
+            } else if (type == 3) {
+                // Double
+                printf("%.2f", real);
             } else {
-                // Print as double or complex
+                // Complex
                 if (imag == 0.0) {
-                    printf("%lf", real);
+                    printf("%d", (int) real);
                 } else if (real == 0.0) {
-                    printf("%lfi", imag);
+                    printf("%di", (int) imag);
                 } else {
-                    printf("%lf+%lfi", real, imag);
+                    printf("%d%+di", (int) real, (int) imag);
                 }
             }
 
